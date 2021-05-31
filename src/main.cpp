1457c585a9bd9d3ffd3d38da81df90c466ffda73
JuneyBoy <arjun.ganesan@ou.edu>
2021-05-31 18:04:11 -0500

    Made flashlight function

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include <Arduino.h>
 
+int ledPin = 0;  // LED connected to digital pin 13
+int inPin = 7;    // pushbutton connected to digital pin 7
+// int buttonPressed = 0;      // variable to store the read value
+int ledState = 0;
+int oldState = 0;
+int newState = 0;
+
 void setup() {
-  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(ledPin, OUTPUT);  // sets the digital pin 13 as output
+  pinMode(inPin, INPUT);    // sets the digital pin 7 as input
+}
+
+void flashlight(){
+  newState = digitalRead(inPin);
+
+  if(newState && newState != oldState){
+    ledState = !ledState;
+    digitalWrite(ledPin,ledState);
+    delay(100);
+  }
+
+  oldState = newState;
+
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN,HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(1000);
+
 }
